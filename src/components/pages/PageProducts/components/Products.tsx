7fe1a8665fe4c124a3d743b7ed4a48cc8165a4c8
sonic-interlock <dzmitry_avdeev@epam.com>
2020-11-15 21:36:57 +0300

    udpate from upstream

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -11,7 +11,7 @@ import {formatAsPrice} from "utils/utils";
 import AddProductToCart from "components/AddProductToCart/AddProductToCart";
 import axios from 'axios';
 import API_PATHS from "constants/apiPaths";
-import productList from "./productList.json";
+// import productList from "./productList.json";
 
 const useStyles = makeStyles((theme) => ({
   card: {
